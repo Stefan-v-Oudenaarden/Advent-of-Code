976e139a6472bd5036c1afd0b80dfd88313e966d
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2025-12-03 10:30:39 +0100

    Added Permutations helper method

--- Sharing is Caring/Helpers.cs
+++ Sharing is Caring/Helpers.cs
@@ -261,5 +261,14 @@ namespace Advent
 
             return false;
         }
+
+        public static IEnumerable<IEnumerable<T>>
+        GetPermutations<T>(IEnumerable<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list.Where(o => !t.Contains(o)),
+                    (t1, t2) => t1.Concat([t2]));
+        }
     }
 }
